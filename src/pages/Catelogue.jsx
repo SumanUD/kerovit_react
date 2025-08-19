55677421f398f19dfdd51411013b73324d973c99
sidilian <sidothapa.dev@gmail.com>
2025-08-19 15:24:52 +0530

    catelogue style change

--- src/pages/Catelogue.jsx
+++ src/pages/Catelogue.jsx
@@ -26,27 +26,6 @@ export const Catelogue = () => {
 
   return (
     <div className="catalogueSingle">
-      <div className="second_banner mobile_view">
-        <div className="inside_banner_content">
-          {/* <h2>the catalogue</h2> */}
-          <img src="./catalogue/year.png" alt="" />
-          <p>Browse our latest catalogue and discover the perfect bathroom solutions for your home.</p>
-        </div>
-        <div className="catalogue">
-
-          {            
-            catelogueData?.categories?.map((item, index)=>(
-              <div className="the_catelogue" key={index}>
-                <a href={item.pdf_link} target="_blank">
-                  <img src={item.thumbnail_image} alt="" loading="lazy" />
-                </a>  
-                <a href={item.pdf_link} target="_blank">{item.title}</a>
-              </div>
-            ))
-          }
-        </div>
-      </div>
-
       <div className="second_banner desktop_view">
         <div className="inside_banner_content">
           {/* <h2>the catalogue</h2> */}
@@ -60,8 +39,7 @@ export const Catelogue = () => {
               <div className="the_catelogue" key={index}>                
                 <a href={item.pdf_link} target="_blank">
                   <img src={item.thumbnail_image} alt="" loading="lazy" />
-                </a>                   
-                <a href={item.pdf_link} target="_blank">{item.title}</a>               
+                </a>                                   
               </div>
             ))
           }
